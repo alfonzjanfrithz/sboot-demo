a5045e6fb5dcb2ccd817770fde81f2a33fcf17bf
alfonzjanfrithz <alfonzjanfrithz@gmail.com>
2022-10-24 00:23:21 +0800

    init

---
+++ src/main/java/com/example/sbootdemo/HelloController.java
@@ -0,0 +1,22 @@
+package com.example.sbootdemo;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/demo")
+public class HelloController {
+    @GetMapping
+    public ResponseEntity getDemo() {
+
+        Map<String, String> map = new HashMap<>();
+        map.put("hello", "world");
+
+        return ResponseEntity.ok(map);
+    }
+}
---
+++ src/main/java/com/example/sbootdemo/SbootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.sbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbootDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbootDemoApplication.class, args);
+    }
+
+}
