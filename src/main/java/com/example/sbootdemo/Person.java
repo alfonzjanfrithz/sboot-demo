ea59f37e7ae867306d5b2e78996bf9736af170bb
alfonzjanfrithz <alfonzjanfrithz@gmail.com>
2022-10-24 18:43:08 +0800

    Add docker file

--- src/main/java/com/example/sbootdemo/Person.java
+++ src/main/java/com/example/sbootdemo/Person.java
@@ -1,12 +1,30 @@
 package com.example.sbootdemo;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 @Entity
+@Getter
+@Builder
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Person {
     @Id
-    Long personId;
+    @Column(name="id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+
     String lastName;
     String firstName;
     String address;
